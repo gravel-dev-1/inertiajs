6e46116f8e19ec324b180ff04c9620e8914e8dce
cramanan <cramananjaonapro@gmail.com>
2026-04-23 13:09:06 +0200

    feat: added public fs serving

--- internal/vite/vite_dev.go
+++ internal/vite/vite_dev.go
@@ -8,10 +8,21 @@ import (
 	"os/exec"
 )
 
-var FS fs.FS = os.DirFS("internal/vite/dev")
-
 func init() {
 	cmd := exec.Command("node", "node_modules/vite/bin/vite")
 	cmd.Stderr, cmd.Stdout = os.Stderr, os.Stdout
 	cmd.Start()
+}
+
+var FS fs.FS = &filesystems{[]fs.FS{os.DirFS("internal/vite/dev"), os.DirFS("public")}}
+
+type filesystems struct{ filesystems []fs.FS }
+
+func (f filesystems) Open(name string) (file fs.File, err error) {
+	for _, filesystem := range f.filesystems {
+		if file, err = filesystem.Open(name); file != nil {
+			return file, err
+		}
+	}
+	return nil, fs.ErrNotExist
 }
